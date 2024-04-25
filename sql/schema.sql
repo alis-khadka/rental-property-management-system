28f5c42c77d042a626b806f1a0f6b2c10e54eaed
saramsh gautam <saramshgautam@saramshs-MacBook-Air.local>
2024-04-25 10:54:39 -0500

    Query to see schema and select queries added

---
+++ sql/schema.sql
@@ -0,0 +1,7 @@
+SELECT column_name FROM information_schema.columns WHERE table_name = 'landlords';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'leases';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'maintenance_requests';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'payments';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'properties';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'tenants';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'violations';
